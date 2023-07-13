3186ab13a52ce039c715fa527d64f464b5e8f641
DrPepper01 <86608383+DrPepper01@users.noreply.github.com>
2023-07-13 21:45:34 +0600

    update
    Добавил html страницы с адаптивным дизайном а так же ссылки на картинки

---
+++ search/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from homes.models import Home
+
+
+def search_views(request):
+    homes = Home.objects.all()
+
+    return render(request, 'search.html', {'homes': homes})
